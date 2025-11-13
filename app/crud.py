e44cf6503a29b6cacbedff503cdba43ea07a2a1b
Wiam12 <71195444+Wiam12@users.noreply.github.com>
2025-11-13 12:08:56 +0100

    Update crud.py

--- app/crud.py
+++ app/crud.py
@@ -24,7 +24,7 @@ def get_user_by_email(db: Session, email: str):
 def get_user_by_username(db: Session, username: str):
     return db.query(models.User).filter(models.User.username == username).first()
 
-def get_user_by_id(db:Session, user_id=int):
+def get_user(db:Session, user_id=int):
     return db.query(models.User).filter(models.User.id == user_id).first()
     
 def create_user(db: Session, user: schemas.UserCreate):
