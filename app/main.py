56d0376a162b6402e188f811276d2b93519b5485
mohamed.mabsout <mabsoutmohamed989@gmail.com>
2025-12-13 17:44:30 +0100

    dcddc

--- app/main.py
+++ app/main.py
@@ -18,7 +18,7 @@ models.Base.metadata.create_all(bind=engine, checkfirst=True)
 app=FastAPI()
 
 origins = ["http://localhost:5173", "http://127.0.0.1:5173"]
-app.add_middleware(CORSMiddleware, allow_origins=origins, allow_credentials=True, allow_methods=["*"], allow_headers=["*"])
+app.add_middleware(CORSMiddleware, allow_origins=origins, allow_credentials=True, allow_methods=["*"], allow_headers=["*"],expose_headers=["Content-Disposition"])
 app.include_router(projects.router)
 app.include_router(users.router)
 app.include_router(auth.router) 
