01e5314919ca59b250e7296de721a8c46f402412
mohamed.mabsout <mabsoutmohamed989@gmail.com>
2026-01-21 12:50:04 +0100

    cdcedswq

--- alembic/versions/ef733b89512b_replace_act_id_with_service_acceptance_.py
+++ alembic/versions/ef733b89512b_replace_act_id_with_service_acceptance_.py
@@ -50,7 +50,7 @@ def upgrade() -> None:
                existing_server_default=sa.text('CURRENT_TIMESTAMP'))
     # op.drop_constraint(op.f('fk_expenses_act'), 'expenses', type_='foreignkey')
     op.create_foreign_key(None, 'expenses', 'service_acceptances', ['service_acceptance_id'], ['id'])
-    op.drop_column('expenses', 'approved_l2_by')
+    # op.drop_column('expenses', 'approved_l2_by')
     op.drop_column('expenses', 'act_id')
     op.drop_column('expenses', 'approved_l2_at')
     op.drop_column('expenses', 'rejection_reason')
